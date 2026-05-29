71132f32cb5f5cb55465b7fc93e1fea240a17335
Abner Lopez <fordlopez777@gmail.com>
2026-05-29 16:55:17 -0600

    3

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ let turno = document.querySelector('#turno')
 let contador = 1
 let mod = 0
 let jugadorO = ''
-
-
-let ganadorTM = "1487"
+let ganadorTM = ""
 let texto = ""
 let tenporalG = ""
 let jugadorX = ''
+let conX=0
+let conO=0
+let conE=0
+let mensaje=""
+let xv=document.querySelector('#xv')
+let ov=document.querySelector('#ov')
+let ev=document.querySelector('#ev')
 let combinacionesGanadoras = [
     '012',
     '345',
@@ -19,9 +24,6 @@ let combinacionesGanadoras = [
     '048',
     '246'
 ];
-
-
-
 
 function ganador() {
     for (let i = 0; i < combinacionesGanadoras.length; i++) {
@@ -37,19 +39,31 @@ function ganador() {
             tenporalG = ""
             for (let k = 0; k < ganador.length; k++) {
 
-                for (let j = 0; j < jugadorX.length; j++) {
+                for (let j = 0; j < ganadorTM.length; j++) {
 
-                    if (jugadorX[j] == ganador[k]) {
-                        tenporalG = tenporalG.concat(ganador[k])
+                    if(texto=="Ganador X"){
+                          if (ganador[k]==ganadorTM[j]) {
+                        tenporalG = tenporalG.concat(jugadorX[j])
+                          console.log(tenporalG+"hola")
                     }
+                    }else{
+                            if (ganador[k]==ganadorTM[j]) {
+                        tenporalG = tenporalG.concat(jugadorO[j])
+                          console.log(tenporalG+"hola")
 
+                    }
+                
+                    }
+  
                 }
             }
-            console.log(tenporalG+"hola")
+          
 
         }
         if (tenporalG == combinacionesGanadoras[i]) {
             contador = -1
+            conX=conX+1
+xv.textContent.
             turno.textContent = texto
             return "ganador" + 1487
             break;
@@ -57,12 +71,7 @@ function ganador() {
         }
 
     }
-
-
-
 }
-
-
 
 console.log(ganador())
 
@@ -81,8 +90,10 @@ console.log("652586+259+215415461654")
                 bot.classList.add('jugador2')
                 turno.textContent = "turno del jugador O"
                 ganadorTM = jugadorX
+                mensaje="victorios X : 0"
                 texto = "Ganador X"
                 console.log(jugadorX+"jugadorx")
+                 bot.disabled = true
                 if(jugadorX.length>=3){
                     ganador()
                 }
@@ -100,6 +111,7 @@ console.log("652586+259+215415461654")
                 ganadorTM = jugadorO
                 texto = "Ganador O"
                 console.log(jugadorX+"jugadorx")
+                  bot.disabled = true
                 if(jugadorX.length>=3){
                     ganador()
                 }
